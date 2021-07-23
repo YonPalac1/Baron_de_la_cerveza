57bb3b6d8d9c1523f16ed8fdc55e0c049b4f8d50
yonPalac1 <yon.palac@gmail.com>
2021-07-23 23:54:23 +0200

    Agrego links en app.js

--- app.js
+++ app.js
@@ -4,9 +4,41 @@ const app = express();
 
 app.use('/', express.static(__dirname + '/public'));
 
+
+// Pagina Home - Yonatan
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/views/index.html'))
 })
+
+// Pagina de detalles de producto - Keila
+app.get('/productDetail', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+})
+
+// Pagina de carrito de compras - Juan
+app.get('/productCart', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+})
+
+// Pagina de configuracion de usuario - Yonatan
+app.get('/register', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+// Pagina de login - Juan
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+// Pagina con datos de contacto - Keila
+app.get('/contact', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/contact.html'))
+})
+// Pagina donde se finalizaria la compra - Yonatan  
+app.get('/finalizePurchase', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/finalizePurchase.html'))
+})
+
 
 app.listen(3030, ()=>{
     console.log("Servidor corriendo")
