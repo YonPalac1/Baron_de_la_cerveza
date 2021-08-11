edc5d4532ca4973de4910cf5b45dc8337202f654
YonPalac1 <yon.palac@gmail.com>
2021-08-11 18:34:56 +0200

    Agregue json

--- el_baron_de_la_cerveza/controllers/indexController.js
+++ el_baron_de_la_cerveza/controllers/indexController.js
@@ -1,5 +1,11 @@
+let { products } = require('../data/dataBase.js');
+
 module.exports = {
 	index: (req, res) => {
-		res.render('index')
+		let destacadosSlider = products.filter(product => product.destacado === true)
+
+		res.render('index', {
+			destacadosSlider
+		})
 	}
 }
