d04d6addd6a27a844eddf2176110117961f82af3
YonPalac1 <yon.palac@gmail.com>
2021-08-26 17:42:29 +0200

    Agregue multer y method override

--- el_baron_de_la_cerveza/src/app.js
+++ el_baron_de_la_cerveza/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+let methodOverride = require('method-override')
 
 
 /*  Enrutadores  */
@@ -13,6 +14,9 @@ let adminRouter = require("./routes/admin/admin.js");
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(methodOverride('_method'))
 
 
 /*  Rutas  */
--- el_baron_de_la_cerveza/src/data/dataBase.js
+++ el_baron_de_la_cerveza/src/data/dataBase.js
@@ -2,5 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-	products: JSON.parse(fs.readFileSync(path.join(__dirname, '/productsDataBase.json'), 'utf-8'))
+	products: JSON.parse(fs.readFileSync(path.join(__dirname, '/productsDataBase.json'), 'utf-8')),
+	writeProductsJSON: (dataBase) => {
+        fs.writeFileSync(path.join(__dirname, "../data/products.json"), JSON.stringify(dataBase), "utf-8")
+    }
 }
